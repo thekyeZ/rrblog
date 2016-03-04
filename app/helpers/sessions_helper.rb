4d5eaa8714f61bc70cfc6ad93b6b038f09ddba52
Przemek Urbaniak <przemek.urbaniak@hotmail.com>
2016-03-04 16:08:13 +0100

    Accout activation done

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -28,7 +28,7 @@ module SessionsHelper
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
+      if user && user.authenticated?(:remember, cookies[:remember_token])
         log_in user
         @current_user = user
       end
@@ -48,7 +48,7 @@ module SessionsHelper
   def store_location
     session[:forwarding_url] = request.url if request.get?
   end
-  
+
   def current_user?(user)
     user == current_user
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,18 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token #todo co to robi ?
+  attr_accessor :remember_token, :activation_token #todo co to robi ?
+
   before_save { self.email = email.downcase }
-  validates :name,  presence: true, length: { maximum: 50 }
+  before_create :create_activation_digest
+
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name,  presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
-
-  has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  has_secure_password
+
 
   # Returns the hash digest of the given string.
   def User.digest(string)
@@ -26,8 +30,24 @@ class User < ActiveRecord::Base
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
-  def authenticated?(remember_token)
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
+
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
   end
 
   def forget
