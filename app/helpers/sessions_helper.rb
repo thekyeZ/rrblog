59dd6f2762c5f7242195b54658a4a6fe195a94d3
Przemek Urbaniak <przemek.urbaniak@hotmail.com>
2016-03-04 13:45:58 +0100

    Deleting, updating users done

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -39,6 +39,18 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
 
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
+  
+  def current_user?(user)
+    user == current_user
+  end
 
 end
