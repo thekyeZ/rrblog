c5ee7614d7e68b4b845f9e63d580ff26c5a3e9b6
Przemek Urbaniak <przemek.urbaniak@hotmail.com>
2016-03-07 09:13:17 +0100

    Password resets

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -24,7 +24,7 @@ module SessionsHelper
   end
 
   def current_user
-    if (user_id = session[:user_id])
+      if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token, :activation_token #todo co to robi ?
+  attr_accessor :remember_token, :activation_token, :reset_token #todo co to robi ?
 
   before_save { self.email = email.downcase }
   before_create :create_activation_digest
@@ -40,6 +40,23 @@ class User < ActiveRecord::Base
     return false if digest.nil?
     BCrypt::Password.new(digest).is_password?(token)
   end
+
+  # Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
 
   def activate
     update_attribute(:activated,    true)
