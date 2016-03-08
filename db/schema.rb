04799cbb4533b335f3b34055e8752282c4323787
Przemek Urbaniak <przemek.urbaniak@hotmail.com>
2016-03-08 12:52:40 +0100

    Following done. 4 test RED from mailers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160308091346) do
+ActiveRecord::Schema.define(version: 20160308094401) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
